1751a219dbcc24857631569ddbc6b6465179a5c8
goalandgo <jj56@rice.edu>
2018-01-23 14:14:41 -0600

    second

---
+++ cmake-build-debug/CMakeFiles/530_projects.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/530_projects.dir/A1/Main/BufferMgr/source/MyDB_BufferManager.cc.o"
+  "CMakeFiles/530_projects.dir/A1/Main/BufferMgr/source/MyDB_Page.cpp.o"
+  "CMakeFiles/530_projects.dir/A1/Main/BufferMgr/source/MyDB_PageHandle.cc.o"
+  "CMakeFiles/530_projects.dir/A1/Main/BufferMgr/source/Node.cpp.o"
+  "CMakeFiles/530_projects.dir/A1/Main/BufferTest/source/BufferQUnit.cc.o"
+  "CMakeFiles/530_projects.dir/A1/Main/Catalog/source/MyDB_Table.cc.o"
+  "530_projects.pdb"
+  "530_projects"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/530_projects.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
